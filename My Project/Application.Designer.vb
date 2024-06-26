da6ac8b02633bdb1e51f263bade463d0d9928996
Mawndy <bulletgun234@gmail.com>
2024-06-26 22:09:31 +0800

    Changed from form1 to frmLogin - StartUp

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.ChronoPulse.Form1
+            Me.MainForm = Global.ChronoPulse.frmLogin
         End Sub
     End Class
 End Namespace
