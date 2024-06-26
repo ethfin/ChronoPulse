6e5ec0d311176faa6910c56a4fbd8514f1270daa
ethfin <christophergalano13@gmail.com>
2024-06-26 21:58:48 +0800

    Update packages

--- frmLogin.Designer.vb
+++ frmLogin.Designer.vb
@@ -22,9 +22,18 @@ Partial Class frmLogin
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SuspendLayout()
+        '
+        'frmLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "frmLogin"
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "frmLogin"
+        Me.Text = "Login"
+        Me.ResumeLayout(False)
+
     End Sub
 End Class
