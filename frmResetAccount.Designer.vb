f947285526d73268cb9d63a38b4d3adfec123fbf
Mawndy <bulletgun234@gmail.com>
2024-07-16 11:55:50 +0800

    Working on Reset Account, first part

--- frmResetAccount.Designer.vb
+++ frmResetAccount.Designer.vb
@@ -28,8 +28,14 @@ Partial Class frmResetAccount
         Me.lblSignUp = New System.Windows.Forms.Label()
         Me.lblCopyright = New System.Windows.Forms.Label()
         Me.Guna2Panel1 = New Guna.UI2.WinForms.Guna2Panel()
-        Me.Guna2PictureBox1 = New Guna.UI2.WinForms.Guna2PictureBox()
         Me.lblWelcome1 = New System.Windows.Forms.Label()
+        Me.Guna2PictureBox1 = New Guna.UI2.WinForms.Guna2PictureBox()
+        Me.lblinfo = New Guna.UI2.WinForms.Guna2HtmlLabel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtEmail = New Guna.UI2.WinForms.Guna2TextBox()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.btnSignUp = New Guna.UI2.WinForms.Guna2Button()
+        Me.lblErrorEmailUsername = New Guna.UI2.WinForms.Guna2HtmlLabel()
         Me.panelLogin2.SuspendLayout()
         Me.Guna2Panel1.SuspendLayout()
         CType(Me.Guna2PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -44,9 +50,9 @@ Partial Class frmResetAccount
         Me.panelLogin2.Controls.Add(Me.lblCopyright)
         Me.panelLogin2.Dock = System.Windows.Forms.DockStyle.Left
         Me.panelLogin2.Location = New System.Drawing.Point(0, 0)
-        Me.panelLogin2.Margin = New System.Windows.Forms.Padding(2)
+        Me.panelLogin2.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.panelLogin2.Name = "panelLogin2"
-        Me.panelLogin2.Size = New System.Drawing.Size(434, 473)
+        Me.panelLogin2.Size = New System.Drawing.Size(579, 582)
         Me.panelLogin2.TabIndex = 8
         '
         'btnMinimize
@@ -61,9 +67,10 @@ Partial Class frmResetAccount
         Me.btnMinimize.ForeColor = System.Drawing.Color.White
         Me.btnMinimize.HoverState.FillColor = System.Drawing.Color.LightSkyBlue
         Me.btnMinimize.IndicateFocus = True
-        Me.btnMinimize.Location = New System.Drawing.Point(374, 0)
+        Me.btnMinimize.Location = New System.Drawing.Point(499, 0)
+        Me.btnMinimize.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.btnMinimize.Name = "btnMinimize"
-        Me.btnMinimize.Size = New System.Drawing.Size(30, 30)
+        Me.btnMinimize.Size = New System.Drawing.Size(40, 37)
         Me.btnMinimize.TabIndex = 6
         Me.btnMinimize.TabStop = False
         Me.btnMinimize.Text = "—"
@@ -75,9 +82,10 @@ Partial Class frmResetAccount
         Me.lblSignUp.BackColor = System.Drawing.Color.Transparent
         Me.lblSignUp.Font = New System.Drawing.Font("Century Gothic", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lblSignUp.ForeColor = System.Drawing.Color.GhostWhite
-        Me.lblSignUp.Location = New System.Drawing.Point(104, 467)
+        Me.lblSignUp.Location = New System.Drawing.Point(139, 575)
+        Me.lblSignUp.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblSignUp.Name = "lblSignUp"
-        Me.lblSignUp.Size = New System.Drawing.Size(0, 17)
+        Me.lblSignUp.Size = New System.Drawing.Size(0, 19)
         Me.lblSignUp.TabIndex = 11
         '
         'lblCopyright
@@ -85,60 +93,164 @@ Partial Class frmResetAccount
         Me.lblCopyright.AutoSize = True
         Me.lblCopyright.BackColor = System.Drawing.Color.Transparent
         Me.lblCopyright.Font = New System.Drawing.Font("Century Gothic", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblCopyright.Location = New System.Drawing.Point(222, 475)
+        Me.lblCopyright.Location = New System.Drawing.Point(296, 585)
+        Me.lblCopyright.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblCopyright.Name = "lblCopyright"
-        Me.lblCopyright.Size = New System.Drawing.Size(212, 16)
+        Me.lblCopyright.Size = New System.Drawing.Size(272, 19)
         Me.lblCopyright.TabIndex = 10
         Me.lblCopyright.Text = "© 2024 ChronoPulse. All rights reserved."
         '
         'Guna2Panel1
         '
+        Me.Guna2Panel1.Controls.Add(Me.lblErrorEmailUsername)
+        Me.Guna2Panel1.Controls.Add(Me.btnSignUp)
+        Me.Guna2Panel1.Controls.Add(Me.LinkLabel1)
+        Me.Guna2Panel1.Controls.Add(Me.txtEmail)
+        Me.Guna2Panel1.Controls.Add(Me.Label1)
+        Me.Guna2Panel1.Controls.Add(Me.lblinfo)
         Me.Guna2Panel1.Controls.Add(Me.lblWelcome1)
         Me.Guna2Panel1.Controls.Add(Me.Guna2PictureBox1)
         Me.Guna2Panel1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Guna2Panel1.Location = New System.Drawing.Point(434, 0)
+        Me.Guna2Panel1.Location = New System.Drawing.Point(579, 0)
+        Me.Guna2Panel1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Guna2Panel1.Name = "Guna2Panel1"
-        Me.Guna2Panel1.Size = New System.Drawing.Size(536, 473)
+        Me.Guna2Panel1.Size = New System.Drawing.Size(714, 582)
         Me.Guna2Panel1.TabIndex = 9
+        '
+        'lblWelcome1
+        '
+        Me.lblWelcome1.BackColor = System.Drawing.Color.Transparent
+        Me.lblWelcome1.Font = New System.Drawing.Font("Century Gothic", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblWelcome1.Location = New System.Drawing.Point(12, 128)
+        Me.lblWelcome1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblWelcome1.Name = "lblWelcome1"
+        Me.lblWelcome1.Size = New System.Drawing.Size(704, 39)
+        Me.lblWelcome1.TabIndex = 10
+        Me.lblWelcome1.Text = "Reset Account"
+        Me.lblWelcome1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'Guna2PictureBox1
         '
         Me.Guna2PictureBox1.BackColor = System.Drawing.Color.Transparent
         Me.Guna2PictureBox1.Image = CType(resources.GetObject("Guna2PictureBox1.Image"), System.Drawing.Image)
         Me.Guna2PictureBox1.ImageRotate = 0!
-        Me.Guna2PictureBox1.Location = New System.Drawing.Point(198, 12)
+        Me.Guna2PictureBox1.Location = New System.Drawing.Point(264, 15)
+        Me.Guna2PictureBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Guna2PictureBox1.Name = "Guna2PictureBox1"
-        Me.Guna2PictureBox1.Size = New System.Drawing.Size(150, 69)
+        Me.Guna2PictureBox1.Size = New System.Drawing.Size(200, 85)
         Me.Guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.Guna2PictureBox1.TabIndex = 1
         Me.Guna2PictureBox1.TabStop = False
         Me.Guna2PictureBox1.UseTransparentBackground = True
         '
-        'lblWelcome1
+        'lblinfo
         '
-        Me.lblWelcome1.BackColor = System.Drawing.Color.Transparent
-        Me.lblWelcome1.Font = New System.Drawing.Font("Century Gothic", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblWelcome1.Location = New System.Drawing.Point(3, 104)
-        Me.lblWelcome1.Name = "lblWelcome1"
-        Me.lblWelcome1.Size = New System.Drawing.Size(528, 32)
-        Me.lblWelcome1.TabIndex = 10
-        Me.lblWelcome1.Text = "Account Login"
-        Me.lblWelcome1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.lblinfo.AutoSize = False
+        Me.lblinfo.BackColor = System.Drawing.Color.Transparent
+        Me.lblinfo.Font = New System.Drawing.Font("Century Gothic", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblinfo.Location = New System.Drawing.Point(149, 184)
+        Me.lblinfo.Name = "lblinfo"
+        Me.lblinfo.Size = New System.Drawing.Size(201, 130)
+        Me.lblinfo.TabIndex = 12
+        Me.lblinfo.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Century Gothic", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(73, 204)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(574, 67)
+        Me.Label1.TabIndex = 14
+        Me.Label1.Text = "We can help you reset your account info and password, first enter your email/user" &
+    "name below."
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txtEmail
+        '
+        Me.txtEmail.BorderColor = System.Drawing.Color.Gray
+        Me.txtEmail.BorderRadius = 2
+        Me.txtEmail.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.txtEmail.DefaultText = ""
+        Me.txtEmail.DisabledState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer))
+        Me.txtEmail.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer))
+        Me.txtEmail.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.txtEmail.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.txtEmail.FocusedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.txtEmail.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Bold)
+        Me.txtEmail.HoverState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.txtEmail.Location = New System.Drawing.Point(95, 322)
+        Me.txtEmail.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtEmail.MaxLength = 62
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.txtEmail.PlaceholderText = "Email/Username"
+        Me.txtEmail.SelectedText = ""
+        Me.txtEmail.Size = New System.Drawing.Size(552, 44)
+        Me.txtEmail.TabIndex = 15
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LinkLabel1.LinkColor = System.Drawing.Color.Gray
+        Me.LinkLabel1.Location = New System.Drawing.Point(7, 553)
+        Me.LinkLabel1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(96, 20)
+        Me.LinkLabel1.TabIndex = 16
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "← Previous"
+        Me.LinkLabel1.VisitedLinkColor = System.Drawing.Color.White
+        '
+        'btnSignUp
+        '
+        Me.btnSignUp.BorderRadius = 2
+        Me.btnSignUp.DisabledState.BorderColor = System.Drawing.Color.DarkGray
+        Me.btnSignUp.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
+        Me.btnSignUp.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.btnSignUp.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.btnSignUp.FillColor = System.Drawing.Color.MediumTurquoise
+        Me.btnSignUp.Font = New System.Drawing.Font("Century Gothic", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSignUp.ForeColor = System.Drawing.Color.White
+        Me.btnSignUp.Location = New System.Drawing.Point(250, 426)
+        Me.btnSignUp.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnSignUp.Name = "btnSignUp"
+        Me.btnSignUp.Size = New System.Drawing.Size(240, 55)
+        Me.btnSignUp.TabIndex = 17
+        Me.btnSignUp.Text = "Next"
+        '
+        'lblErrorEmailUsername
+        '
+        Me.lblErrorEmailUsername.AutoSize = False
+        Me.lblErrorEmailUsername.BackColor = System.Drawing.Color.Transparent
+        Me.lblErrorEmailUsername.Font = New System.Drawing.Font("Century Gothic", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblErrorEmailUsername.ForeColor = System.Drawing.Color.Red
+        Me.lblErrorEmailUsername.Location = New System.Drawing.Point(306, 375)
+        Me.lblErrorEmailUsername.Margin = New System.Windows.Forms.Padding(4)
+        Me.lblErrorEmailUsername.Name = "lblErrorEmailUsername"
+        Me.lblErrorEmailUsername.Size = New System.Drawing.Size(341, 34)
+        Me.lblErrorEmailUsername.TabIndex = 43
+        Me.lblErrorEmailUsername.TabStop = False
+        Me.lblErrorEmailUsername.Text = "Error"
+        Me.lblErrorEmailUsername.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter
+        Me.lblErrorEmailUsername.Visible = False
         '
         'frmResetAccount
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
-        Me.ClientSize = New System.Drawing.Size(970, 473)
+        Me.ClientSize = New System.Drawing.Size(1293, 582)
         Me.Controls.Add(Me.Guna2Panel1)
         Me.Controls.Add(Me.panelLogin2)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "frmResetAccount"
         Me.Text = "frmResetAccount"
         Me.panelLogin2.ResumeLayout(False)
         Me.panelLogin2.PerformLayout()
         Me.Guna2Panel1.ResumeLayout(False)
+        Me.Guna2Panel1.PerformLayout()
         CType(Me.Guna2PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
@@ -151,4 +263,10 @@ Partial Class frmResetAccount
     Friend WithEvents Guna2Panel1 As Guna.UI2.WinForms.Guna2Panel
     Friend WithEvents Guna2PictureBox1 As Guna.UI2.WinForms.Guna2PictureBox
     Friend WithEvents lblWelcome1 As Label
+    Friend WithEvents lblinfo As Guna.UI2.WinForms.Guna2HtmlLabel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtEmail As Guna.UI2.WinForms.Guna2TextBox
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents btnSignUp As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents lblErrorEmailUsername As Guna.UI2.WinForms.Guna2HtmlLabel
 End Class
