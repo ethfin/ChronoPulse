ad4e705d3a695a995d17af22c05a97848ff7bade
ethfin <christophergalano13@gmail.com>
2024-07-18 20:57:34 +0800

    optimized the common sql connection

--- Common.vb
+++ Common.vb
@@ -1,17 +1,17 @@
 ﻿Imports MySql.Data.MySqlClient
 
 Public Class Common
-    Public Shared myDBConnection As MySqlConnection
+    Private Shared myDBConnection As MySqlConnection
 
     Public Shared Function getDBConnectionX() As MySqlConnection
-
-        myDBConnection = New MySqlConnection(
-               "Database=projectDB;" &
-                     "Data Source=localhost;" &
-                    "User id= projectDBAdmin;" &
-                    "Password= 1322LDSP;" &
-                    "Port=3306;Command TimeOut=600;")
+        If myDBConnection Is Nothing Then
+            myDBConnection = New MySqlConnection(
+                "Database=projectDB;" &
+                "Data Source=localhost;" &
+                "User id=projectDBAdmin;" &
+                "Password=1322LDSP;" &
+                "Port=3306;Command Timeout=600;")
+        End If
         Return myDBConnection
-
     End Function
 End Class
