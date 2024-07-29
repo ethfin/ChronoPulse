a629ea575ab5de98fca7f9b9c1717905523ca832
ethfin <christophergalano13@gmail.com>
2024-07-29 19:51:43 +0800

    Add new components and rearrange frmDashboard

--- mainForms/frmDashboard.Designer.vb
+++ mainForms/frmDashboard.Designer.vb
@@ -23,12 +23,15 @@ Partial Class frmDashboard
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Guna2Panel1 = New Guna.UI2.WinForms.Guna2Panel()
-        Me.Guna2Panel2 = New Guna.UI2.WinForms.Guna2Panel()
-        Me.Guna2HtmlLabel1 = New Guna.UI2.WinForms.Guna2HtmlLabel()
-        Me.Guna2HtmlLabel2 = New Guna.UI2.WinForms.Guna2HtmlLabel()
-        Me.Guna2Panel3 = New Guna.UI2.WinForms.Guna2Panel()
-        Me.Guna2Panel4 = New Guna.UI2.WinForms.Guna2Panel()
         Me.Guna2Panel5 = New Guna.UI2.WinForms.Guna2Panel()
+        Me.GunaAreaDataset1 = New Guna.Charts.WinForms.GunaAreaDataset()
+        Me.GunaAreaDataset2 = New Guna.Charts.WinForms.GunaAreaDataset()
+        Me.GunaAreaDataset3 = New Guna.Charts.WinForms.GunaAreaDataset()
+        Me.Guna2Panel4 = New Guna.UI2.WinForms.Guna2Panel()
+        Me.Guna2Panel3 = New Guna.UI2.WinForms.Guna2Panel()
+        Me.Guna2Panel2 = New Guna.UI2.WinForms.Guna2Panel()
+        Me.Guna2HtmlLabel2 = New Guna.UI2.WinForms.Guna2HtmlLabel()
+        Me.Guna2HtmlLabel1 = New Guna.UI2.WinForms.Guna2HtmlLabel()
         Me.Guna2Panel1.SuspendLayout()
         Me.Guna2Panel2.SuspendLayout()
         Me.SuspendLayout()
@@ -46,57 +49,38 @@ Partial Class frmDashboard
         Me.Guna2Panel1.Size = New System.Drawing.Size(1248, 652)
         Me.Guna2Panel1.TabIndex = 0
         '
-        'Guna2Panel2
+        'Guna2Panel5
         '
-        Me.Guna2Panel2.BackColor = System.Drawing.Color.Transparent
-        Me.Guna2Panel2.BorderColor = System.Drawing.Color.FromArgb(CType(CType(53, Byte), Integer), CType(CType(57, Byte), Integer), CType(CType(69, Byte), Integer))
-        Me.Guna2Panel2.BorderThickness = 1
-        Me.Guna2Panel2.Controls.Add(Me.Guna2HtmlLabel2)
-        Me.Guna2Panel2.Controls.Add(Me.Guna2HtmlLabel1)
-        Me.Guna2Panel2.Location = New System.Drawing.Point(55, 55)
-        Me.Guna2Panel2.Margin = New System.Windows.Forms.Padding(5)
-        Me.Guna2Panel2.Name = "Guna2Panel2"
-        Me.Guna2Panel2.Padding = New System.Windows.Forms.Padding(25)
-        Me.Guna2Panel2.Size = New System.Drawing.Size(345, 241)
-        Me.Guna2Panel2.TabIndex = 1
-        '
-        'Guna2HtmlLabel1
-        '
-        Me.Guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent
-        Me.Guna2HtmlLabel1.Font = New System.Drawing.Font("DM Sans 14pt", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Guna2HtmlLabel1.ForeColor = System.Drawing.Color.White
-        Me.Guna2HtmlLabel1.Location = New System.Drawing.Point(30, 30)
-        Me.Guna2HtmlLabel1.Margin = New System.Windows.Forms.Padding(0)
-        Me.Guna2HtmlLabel1.Name = "Guna2HtmlLabel1"
-        Me.Guna2HtmlLabel1.Size = New System.Drawing.Size(72, 22)
-        Me.Guna2HtmlLabel1.TabIndex = 0
-        Me.Guna2HtmlLabel1.Text = "BALANCE:"
-        '
-        'Guna2HtmlLabel2
-        '
-        Me.Guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent
-        Me.Guna2HtmlLabel2.Font = New System.Drawing.Font("Inter", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Guna2HtmlLabel2.ForeColor = System.Drawing.Color.White
-        Me.Guna2HtmlLabel2.Location = New System.Drawing.Point(30, 52)
-        Me.Guna2HtmlLabel2.Margin = New System.Windows.Forms.Padding(0)
-        Me.Guna2HtmlLabel2.Name = "Guna2HtmlLabel2"
-        Me.Guna2HtmlLabel2.Size = New System.Drawing.Size(79, 41)
-        Me.Guna2HtmlLabel2.TabIndex = 1
-        Me.Guna2HtmlLabel2.Text = "0.00"
-        '
-        'Guna2Panel3
-        '
-        Me.Guna2Panel3.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+        Me.Guna2Panel5.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.Guna2Panel3.BackColor = System.Drawing.Color.Transparent
-        Me.Guna2Panel3.BorderColor = System.Drawing.Color.FromArgb(CType(CType(53, Byte), Integer), CType(CType(57, Byte), Integer), CType(CType(69, Byte), Integer))
-        Me.Guna2Panel3.BorderThickness = 1
-        Me.Guna2Panel3.Location = New System.Drawing.Point(410, 55)
-        Me.Guna2Panel3.Margin = New System.Windows.Forms.Padding(5)
-        Me.Guna2Panel3.Name = "Guna2Panel3"
-        Me.Guna2Panel3.Padding = New System.Windows.Forms.Padding(25)
-        Me.Guna2Panel3.Size = New System.Drawing.Size(345, 241)
-        Me.Guna2Panel3.TabIndex = 2
+        Me.Guna2Panel5.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2Panel5.BorderColor = System.Drawing.Color.FromArgb(CType(CType(53, Byte), Integer), CType(CType(57, Byte), Integer), CType(CType(69, Byte), Integer))
+        Me.Guna2Panel5.BorderThickness = 1
+        Me.Guna2Panel5.Location = New System.Drawing.Point(55, 306)
+        Me.Guna2Panel5.Margin = New System.Windows.Forms.Padding(5)
+        Me.Guna2Panel5.Name = "Guna2Panel5"
+        Me.Guna2Panel5.Padding = New System.Windows.Forms.Padding(25)
+        Me.Guna2Panel5.Size = New System.Drawing.Size(700, 291)
+        Me.Guna2Panel5.TabIndex = 2
+        '
+        'GunaAreaDataset1
+        '
+        Me.GunaAreaDataset1.BorderColor = System.Drawing.Color.Empty
+        Me.GunaAreaDataset1.FillColor = System.Drawing.Color.Empty
+        Me.GunaAreaDataset1.Label = "Area1"
+        '
+        'GunaAreaDataset2
+        '
+        Me.GunaAreaDataset2.BorderColor = System.Drawing.Color.Empty
+        Me.GunaAreaDataset2.FillColor = System.Drawing.Color.Empty
+        Me.GunaAreaDataset2.Label = "Area2"
+        '
+        'GunaAreaDataset3
+        '
+        Me.GunaAreaDataset3.BorderColor = System.Drawing.Color.Empty
+        Me.GunaAreaDataset3.FillColor = System.Drawing.Color.Empty
+        Me.GunaAreaDataset3.Label = "Area3"
         '
         'Guna2Panel4
         '
@@ -112,20 +96,57 @@ Partial Class frmDashboard
         Me.Guna2Panel4.Size = New System.Drawing.Size(428, 542)
         Me.Guna2Panel4.TabIndex = 3
         '
-        'Guna2Panel5
+        'Guna2Panel3
         '
-        Me.Guna2Panel5.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
+        Me.Guna2Panel3.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.Guna2Panel5.BackColor = System.Drawing.Color.Transparent
-        Me.Guna2Panel5.BorderColor = System.Drawing.Color.FromArgb(CType(CType(53, Byte), Integer), CType(CType(57, Byte), Integer), CType(CType(69, Byte), Integer))
-        Me.Guna2Panel5.BorderThickness = 1
-        Me.Guna2Panel5.Location = New System.Drawing.Point(55, 306)
-        Me.Guna2Panel5.Margin = New System.Windows.Forms.Padding(5)
-        Me.Guna2Panel5.Name = "Guna2Panel5"
-        Me.Guna2Panel5.Padding = New System.Windows.Forms.Padding(25)
-        Me.Guna2Panel5.Size = New System.Drawing.Size(700, 291)
-        Me.Guna2Panel5.TabIndex = 2
+        Me.Guna2Panel3.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2Panel3.BorderColor = System.Drawing.Color.FromArgb(CType(CType(53, Byte), Integer), CType(CType(57, Byte), Integer), CType(CType(69, Byte), Integer))
+        Me.Guna2Panel3.BorderThickness = 1
+        Me.Guna2Panel3.Location = New System.Drawing.Point(410, 55)
+        Me.Guna2Panel3.Margin = New System.Windows.Forms.Padding(5)
+        Me.Guna2Panel3.Name = "Guna2Panel3"
+        Me.Guna2Panel3.Padding = New System.Windows.Forms.Padding(25)
+        Me.Guna2Panel3.Size = New System.Drawing.Size(345, 241)
+        Me.Guna2Panel3.TabIndex = 2
+        '
+        'Guna2Panel2
+        '
+        Me.Guna2Panel2.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2Panel2.BorderColor = System.Drawing.Color.FromArgb(CType(CType(53, Byte), Integer), CType(CType(57, Byte), Integer), CType(CType(69, Byte), Integer))
+        Me.Guna2Panel2.BorderThickness = 1
+        Me.Guna2Panel2.Controls.Add(Me.Guna2HtmlLabel2)
+        Me.Guna2Panel2.Controls.Add(Me.Guna2HtmlLabel1)
+        Me.Guna2Panel2.Location = New System.Drawing.Point(55, 55)
+        Me.Guna2Panel2.Margin = New System.Windows.Forms.Padding(5)
+        Me.Guna2Panel2.Name = "Guna2Panel2"
+        Me.Guna2Panel2.Padding = New System.Windows.Forms.Padding(25)
+        Me.Guna2Panel2.Size = New System.Drawing.Size(345, 241)
+        Me.Guna2Panel2.TabIndex = 1
+        '
+        'Guna2HtmlLabel2
+        '
+        Me.Guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2HtmlLabel2.Font = New System.Drawing.Font("Inter", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Guna2HtmlLabel2.ForeColor = System.Drawing.Color.White
+        Me.Guna2HtmlLabel2.Location = New System.Drawing.Point(30, 52)
+        Me.Guna2HtmlLabel2.Margin = New System.Windows.Forms.Padding(0)
+        Me.Guna2HtmlLabel2.Name = "Guna2HtmlLabel2"
+        Me.Guna2HtmlLabel2.Size = New System.Drawing.Size(79, 41)
+        Me.Guna2HtmlLabel2.TabIndex = 1
+        Me.Guna2HtmlLabel2.Text = "0.00"
+        '
+        'Guna2HtmlLabel1
+        '
+        Me.Guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2HtmlLabel1.Font = New System.Drawing.Font("DM Sans 14pt", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Guna2HtmlLabel1.ForeColor = System.Drawing.Color.White
+        Me.Guna2HtmlLabel1.Location = New System.Drawing.Point(30, 30)
+        Me.Guna2HtmlLabel1.Margin = New System.Windows.Forms.Padding(0)
+        Me.Guna2HtmlLabel1.Name = "Guna2HtmlLabel1"
+        Me.Guna2HtmlLabel1.Size = New System.Drawing.Size(72, 22)
+        Me.Guna2HtmlLabel1.TabIndex = 0
+        Me.Guna2HtmlLabel1.Text = "BALANCE:"
         '
         'frmDashboard
         '
@@ -153,4 +174,7 @@ Partial Class frmDashboard
     Friend WithEvents Guna2Panel4 As Guna.UI2.WinForms.Guna2Panel
     Friend WithEvents Guna2Panel3 As Guna.UI2.WinForms.Guna2Panel
     Friend WithEvents Guna2Panel5 As Guna.UI2.WinForms.Guna2Panel
+    Friend WithEvents GunaAreaDataset1 As Guna.Charts.WinForms.GunaAreaDataset
+    Friend WithEvents GunaAreaDataset2 As Guna.Charts.WinForms.GunaAreaDataset
+    Friend WithEvents GunaAreaDataset3 As Guna.Charts.WinForms.GunaAreaDataset
 End Class
