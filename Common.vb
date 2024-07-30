a4d8bde8a8c32d8af8b0e4dc8ef72a60fdaadbf8
ethfin <christophergalano13@gmail.com>
2024-07-30 10:50:14 +0800

    Refactor DB connection initialization in Common.vb
    Updated the Common class to change myDBConnection from a Lazy(Of MySqlConnection) to a regular MySqlConnection. Modified getDBConnectionX to initialize myDBConnection if it is Nothing. Removed redundant commented-out code at the end of the file.

--- Common.vb
+++ Common.vb
@@ -1,34 +1,17 @@
 ﻿Imports MySql.Data.MySqlClient
 
 Public Class Common
-    Private Shared ReadOnly myDBConnection As New Lazy(Of MySqlConnection)(
-        Function() New MySqlConnection(
-            "Database=projectDB;" &
-            "Data Source=localhost;" &
-            "User id=projectDBAdmin;" &
-            "Password=1322LDSP;" &
-            "Port=3306;Command Timeout=600;"))
+    Private Shared myDBConnection As MySqlConnection
 
     Public Shared Function getDBConnectionX() As MySqlConnection
-        Return myDBConnection.Value
+        If myDBConnection Is Nothing Then
+            myDBConnection = New MySqlConnection(
+                "Database=projectDB;" &
+                "Data Source=localhost;" &
+                "User id=projectDBAdmin;" &
+                "Password=1322LDSP;" &
+                "Port=3306;Command Timeout=600;")
+        End If
+        Return myDBConnection
     End Function
 End Class
-
-
-'Imports MySql.Data.MySqlClient
-
-'Public Class Common
-'    Private Shared myDBConnection As MySqlConnection
-
-'    Public Shared Function getDBConnectionX() As MySqlConnection
-'        If myDBConnection Is Nothing Then
-'            myDBConnection = New MySqlConnection(
-'                "Database=projectDB;" &
-'                "Data Source=localhost;" &
-'                "User id=projectDBAdmin;" &
-'                "Password=1322LDSP;" &
-'                "Port=3306;Command Timeout=600;")
-'        End If
-'        Return myDBConnection
-'    End Function
-'End Class
