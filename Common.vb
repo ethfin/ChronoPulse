668cb28b1070761ad453a1ad9a35de51e5577309
ethfin <christophergalano13@gmail.com>
2024-07-30 10:48:41 +0800

    SQL connection test

--- Common.vb
+++ Common.vb
@@ -1,17 +1,34 @@
 ﻿Imports MySql.Data.MySqlClient
 
 Public Class Common
-    Private Shared myDBConnection As MySqlConnection
+    Private Shared ReadOnly myDBConnection As New Lazy(Of MySqlConnection)(
+        Function() New MySqlConnection(
+            "Database=projectDB;" &
+            "Data Source=localhost;" &
+            "User id=projectDBAdmin;" &
+            "Password=1322LDSP;" &
+            "Port=3306;Command Timeout=600;"))
 
     Public Shared Function getDBConnectionX() As MySqlConnection
-        If myDBConnection Is Nothing Then
-            myDBConnection = New MySqlConnection(
-                "Database=projectDB;" &
-                "Data Source=localhost;" &
-                "User id=projectDBAdmin;" &
-                "Password=1322LDSP;" &
-                "Port=3306;Command Timeout=600;")
-        End If
-        Return myDBConnection
+        Return myDBConnection.Value
     End Function
 End Class
+
+
+'Imports MySql.Data.MySqlClient
+
+'Public Class Common
+'    Private Shared myDBConnection As MySqlConnection
+
+'    Public Shared Function getDBConnectionX() As MySqlConnection
+'        If myDBConnection Is Nothing Then
+'            myDBConnection = New MySqlConnection(
+'                "Database=projectDB;" &
+'                "Data Source=localhost;" &
+'                "User id=projectDBAdmin;" &
+'                "Password=1322LDSP;" &
+'                "Port=3306;Command Timeout=600;")
+'        End If
+'        Return myDBConnection
+'    End Function
+'End Class
